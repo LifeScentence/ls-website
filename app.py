69759e85a32ff61b2b8764a1958991fecdd1982a
Life Scentence <149612803+LifeScentence@users.noreply.github.com>
2023-11-01 19:32:43 +0100

    commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="Home - LS", page_icon=":love_letter:", layout="wide")
+
+# ---- HEADER SECTION ----
+st.subheader("Welcome, we're Life Scentence! :wave:")
+st.title("A healthcare company at your service!")
+st.write("Get your healthcare here and get updated when anything changes.")
+st.write("[Learn More](https://trollface.dk/)")
