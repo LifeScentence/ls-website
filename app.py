ca472e2e58381ad1dcca71cef4641bea5f9a5840
Life Scentence <149612803+LifeScentence@users.noreply.github.com>
2023-11-01 19:41:08 +0100

    Added - What do we do

--- app.py
+++ app.py
@@ -4,7 +4,21 @@ import streamlit as st
 st.set_page_config(page_title="Home - LS", page_icon=":love_letter:", layout="wide")
 
 # ---- HEADER SECTION ----
-st.subheader("Welcome, we're Life Scentence! :wave:")
-st.title("A healthcare company at your service!")
-st.write("Get your healthcare here and get updated when anything changes.")
-st.write("[Learn More](https://trollface.dk/)")
+with st.container():
+    st.subheader("Welcome, we're Life Scentence! :wave:")
+    st.title("A healthcare company at your service!")
+    st.write("Get your healthcare here and get updated when anything changes.")
+    st.write("[Learn More](https://trollface.dk/)")
+
+# --- WHAT WE DO ----
+with st.container():
+    st.write("---")
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.header("What we do")
+        st.write("##")
+        st.write(
+            """
+            We make sure you get the best healthcare anyone can provide and help you with all of your questions.        
+            """
+        )
